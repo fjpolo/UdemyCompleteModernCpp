11038bc864c3a4bd6840baf45f45e27c3bb4c7be
franco.polo <franco.polo@ii-vi.com>
2021-04-12 09:42:51 +0200

    Section11 incomplete due to incompatibilities from MinGW.-

---
+++ VSC/Section11/001RawStringLiterals/build/CMakeFiles/mainApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainApp.dir/src/main.cpp.obj"
+  "libmainApp.dll.a"
+  "mainApp.exe"
+  "mainApp.exe.manifest"
+  "mainApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ VSC/Section11/003CopyUtility/src/main.cpp
@@ -0,0 +1,41 @@
+/**
+ * Includes
+ */ 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <experimental\filesystem>
+
+/**
+ * \class
+ * 
+ * \brief
+ */ 
+
+
+/**
+ * \fn Write
+ * 
+ * \brief
+ */ 
+
+
+
+/**
+ * \fn main
+ * 
+ * \brief
+ */
+int main(){
+
+    /**
+     * Video 116
+     */ 
+    using namespace std::filesystem;
+
+
+
+
+    return 0;
+}
+ 
